1992e44bb84376b76e50381be68a55caf5d57781
ali1ve <aliaga.yahya@gmail.com>
2024-11-11 17:38:12 +0400

    homeWork

---
+++ app.js
@@ -0,0 +1,13 @@
+let rublWallet = 1200
+let dollarValue = 1
+let dollarWallet = 20
+
+function userWallet(){
+    if (dollarValue == 1){
+        console.log((dollarWallet*75)+rublWallet);
+    }else{
+        console.log("duzgun yazmamisiz");
+        
+    }
+}
+userWallet()
